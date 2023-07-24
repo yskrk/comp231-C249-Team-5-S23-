edf9f84be938de8ac8468c2152dadc6a1089bb31
brentongalien981 <brentongalien981@gmail.com>
2023-07-24 17:05:40 -0400

    Re-organizing our main project folder.

--- config/env/development.js
+++
@@ -1,7 +0,0 @@
-﻿//Development configuration options
-//To sign the session identifier, use a secret string
-module.exports = {
-    db: 'mongodb://localhost/articles-db',
-    sessionSecret: 'developmentSessionSecret',
-    secretKey: 'real_secret'
-};
