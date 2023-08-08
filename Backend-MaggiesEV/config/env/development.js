16d8d9a1992f93dcf1445198adbf6ad845b17fe2
Yusuke <ykuroki@my.centennialcollege.ca>
2023-08-08 01:19:37 -0400

    Added some files for backend

---
+++ Backend-MaggiesEV/config/env/development.js
@@ -0,0 +1,7 @@
+﻿//Development configuration options
+//To sign the session identifier, use a secret string
+module.exports = {
+    db: 'mongodb://localhost/articles-db',
+    sessionSecret: 'developmentSessionSecret',
+    secretKey: 'real_secret'
+};
