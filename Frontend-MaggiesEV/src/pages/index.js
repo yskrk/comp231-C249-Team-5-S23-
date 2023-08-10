361a551ce6f129d41a4e67999aa1e21a598660ec
brentongalien981 <brentongalien981@gmail.com>
2023-08-10 14:45:57 -0400

    update

--- Frontend-MaggiesEV/src/pages/index.js
+++ Frontend-MaggiesEV/src/pages/index.js
@@ -30,60 +30,19 @@ export async function getStaticProps() {
 const Index = () => {
 	return (
 		<React.Fragment>
-			<Swiper
-				data={data.swiper}
-				autoplay={{
-					delay: 5000,
-				}}
-				loop
-				centeredSlides
-				speed={1500}
-				parallax
-				navigation
-				pagination
-				style={{ height: "95vh", minHeight: "600px" }}
-				containerclass="px-lg-7"
-			/>
-			{data.maincategories && (
-				<div className="bg-gray-100 position-relative">
-					<Container className="py-6">
-						<Row>
-							{data.maincategories.map((category) => (
-								<Col key={category.name} sm="6" className="mb-5 mb-sm-0">
-									<Card className="card-scale shadow-0 border-0 text-white text-hover-gray-900 overlay-hover-light text-center">
-										<div>
-											<Image
-												className="img-scale card-img"
-												src={category.img}
-												alt={category.name}
-												width={630}
-												height={807}
-												sizes="(max-width: 576px) calc(100vw - 30px), 50vw"
-											/>
-										</div>
-										<Card.ImgOverlay className="d-flex align-items-center">
-											<div className="w-100 py-3">
-												<h2 className="display-3 fw-bold mb-0">
-													{category.name}
-												</h2>
-												<Link href={category.link}>
-													<a className="stretched-link">
-														<span className="sr-only">{category.button}</span>
-													</a>
-												</Link>
-											</div>
-										</Card.ImgOverlay>
-									</Card>
-								</Col>
-							))}
-						</Row>
-					</Container>
-				</div>
-			)}
-			<NewArrivals fluid headCenter products={products} />
-			<Sale className="py-6" backgroundImage="/img/photo/deal-plain.jpg" />
-			<OurHistory />
-			<Brands className="pb-6" />
+			<div className="bg-gray-100 position-relative">
+				<Container className="py-6">
+					<Row>
+						<Col sm="6" className="mb-5 mb-sm-0">
+							<Card className="card-scale shadow-0 border-0 text-hover-gray-900 overlay-hover-light text-center">
+								<div>
+									<h1>TODO: Maggie's EVs - Homepage</h1>
+								</div>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</div>
 		</React.Fragment>
 	)
 }
