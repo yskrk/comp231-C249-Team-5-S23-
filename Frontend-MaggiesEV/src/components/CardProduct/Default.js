7036139359ad0ac50e2f97244b7cb0860f5459bc
brentongalien981 <brentongalien981@gmail.com>
2023-08-15 12:32:01 -0400

    update

--- Frontend-MaggiesEV/src/components/CardProduct/Default.js
+++ Frontend-MaggiesEV/src/components/CardProduct/Default.js
@@ -68,7 +68,7 @@ const CardProductDefault = ({
 						<img
 							style={{
 								width: "100%",
-								height: "523px",
+								height: "350px",
 								objectFit: "contain",
 								backgroundColor: "rgba(0, 0, 0, 0.4)"
 							}}
